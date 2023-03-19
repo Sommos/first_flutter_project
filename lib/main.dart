ae459ce347fd118e64e80d4e39e9e590d62e9b21
Sommos <Sommosgaming@gmail.com>
2023-03-19 15:39:58 +0000

    added question.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,66 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget 
+{
+  @override
+  State<StatefulWidget> createState() 
+  {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> 
+{
+  var _questionIndex = 0;
+
+  void _answerQuestion() 
+  {
+    setState(() 
+    {
+      _questionIndex += 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            OutlinedButton(
+              child: Text("Answer 1"), 
+              onPressed: _answerQuestion
+            ),
+            OutlinedButton(
+              child: Text("Answer 2"), 
+              onPressed: () => print("Answer 2 chosen!")
+            ),
+            OutlinedButton(
+              child: Text("Answer 3"), 
+              onPressed: () => print("Answer 3 chosen!")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 // void main()
 // {
@@ -17,61 +77,6 @@ void main() => runApp(MyApp());
 //   print(firstResult);
 //   print("Hello!");
 // }
-
-class MyApp extends StatefulWidget 
-{
-  @override
-  State<StatefulWidget> createState() 
-  {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> 
-{
-  var questionIndex = 0;
-
-  void answerQuestion() 
-  {
-    setState(() 
-    {
-      questionIndex += 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var questions = [
-      "What's your favorite color?",
-      "What's your favorite animal?",
-    ];
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-        ),
-        body: Column(
-          children: [
-            Text(questions[questionIndex]),
-            OutlinedButton(
-              child: Text("Answer 1"), 
-              onPressed: answerQuestion
-            ),
-            OutlinedButton(
-              child: Text("Answer 2"), 
-              onPressed: () => print("Answer 2 chosen!")
-            ),
-            OutlinedButton(
-              child: Text("Answer 3"), 
-              onPressed: () => print("Answer 3 chosen!")
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 // class Person {
 //   String name;
